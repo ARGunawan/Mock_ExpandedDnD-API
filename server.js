07e221ef784a31ff9775b03a34722916b68966a1
Alfonso <argunwan@gmail.com>
2022-05-10 14:03:59 -0400

    removed implementation

--- server.js
+++ server.js
@@ -8,16 +8,3 @@ server.use(middlewares);
 server.use(router);
 
 server.listen(port)
-
-window.fetch('__rules')
-  .then(response => {
-    if (!response.ok) {
-      throw Error(response.statusText); 
-    } else { 
-      return response.ok; 
-    }
-  })
-  .then(response => response.json())
-  .then(customRoutes => document.getElementById('custom-routes').innerHTML = CustomRoutesBlock({
-    customRoutes}))
-  .catch(error => console.log(error));
